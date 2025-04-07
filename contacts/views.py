2d8dc2c1d6ccf0b1fe31516eaa552a2b664ffc9e
TharHtet236401 <tharhteta3@gmail.com>
2025-04-07 16:59:34 +0700

    Implement search functionality for contacts with error handling

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 # Create your views here.
 @login_required
@@ -9,3 +10,20 @@ def index(request):
         "contacts": contacts
     }
     return render(request, 'contacts.html', context)
+
+
+@login_required
+def search_contacts(request):   
+    search_query = request.GET.get('search')
+    q_object = Q()
+    if search_query:
+        try:
+            q_object = Q(name__icontains=search_query) | Q(email__icontains=search_query)
+            contacts = request.user.contacts.filter(q_object).order_by("-created_at")
+        except Exception as e:
+            contacts = request.user.contacts.all().order_by("-created_at")
+            # Optionally log the exception e
+    context = {
+        "contacts": contacts
+    }
+    return render(request, 'partials/contacts-list.html', context)
