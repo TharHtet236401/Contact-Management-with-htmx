a537458b6b799cc79bbbd010b4c9b6d428736e87
TharHtet236401 <tharhteta3@gmail.com>
2025-04-21 10:52:52 +0700

    Update Contact model to allow additional file types for document uploads: added 'ppt' and 'pptx' to allowed extensions.

--- contacts/models.py
+++ contacts/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=255)
-    document = models.FileField(upload_to='contact_docs/', null=True, blank=True, validators=[FileExtensionValidator(allowed_extensions=['pdf','docx','doc','txt'])])
+    document = models.FileField(upload_to='contact_docs/', null=True, blank=True, validators=[FileExtensionValidator(allowed_extensions=['pdf','docx','doc','txt','ppt','pptx'])])
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="contacts") #we will use this to link the contact to the user
 
